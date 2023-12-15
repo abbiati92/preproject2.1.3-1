ba2a7a8d832a2a6ee44b40daee4925b6e884f1d7
abbiati92 <superkika@yandex.ru>
2023-12-15 16:30:39 +0300

    commit1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getName());
+
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getName());
+
+        System.out.println(bean==bean1);
+        System.out.println(catBean==catBean1);
     }
 }
